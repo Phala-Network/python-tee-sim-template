ce3c883a073f44c6ff5c78e9f7222e4f5b6ed136
HashWarlock <hashwarlock@protonmail.com>
2024-11-07 19:46:26 -0600

    Update app.py to create api call for derive key and tdx quote

--- app.py
+++ app.py
@@ -3,16 +3,24 @@ from dstack_sdk import AsyncTappdClient, DeriveKeyResponse, TdxQuoteResponse
 from fastapi import FastAPI
 
 app = FastAPI()
-# endpoint = '../../tappd.sock'
-endpoint = 'http://host.docker.internal:8090'
 
 @app.get("/")
 async def root():
+    return {"message": "The World! Call /derivekey or /tdxquote"}
+
+@app.get("/derivekey")
+async def derivekey():
     client = AsyncTappdClient()
     deriveKey = await client.derive_key('/', 'test')
     assert isinstance(deriveKey, DeriveKeyResponse)
     asBytes = deriveKey.toBytes()
     assert isinstance(asBytes, bytes)
     limitedSize = deriveKey.toBytes(32)
+    return {"deriveKey": asBytes.hex(), "derive_32bytes": limitedSize.hex()}
+    
+@app.get("/tdxquote")
+async def tdxquote():
+    client = AsyncTappdClient()
     tdxQuote = await client.tdx_quote('test')
-    return {"deriveKey": asBytes.hex(), "derive_32bytes": limitedSize.hex(), "tdxQuote": tdxQuote}
+    assert isinstance(tdxQuote, TdxQuoteResponse)
+    return {"tdxQuote": tdxQuote}
